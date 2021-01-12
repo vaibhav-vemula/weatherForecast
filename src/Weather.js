82912b6c8688558897a6ac666dc444c4a7b2ba7a
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2021-01-12 23:33:39 +0530

    Static location added when location is unavailable

--- src/Weather.js
+++ src/Weather.js
@@ -151,6 +151,57 @@ class Weather extends React.Component {
           weatherInfo: null,
         });
       });
+      },error =>{
+
+        const weather = `https://api.openweathermap.org/data/2.5/weather?q=bangalore&APPID=${APIkey}&units=metric`;
+    const forecast = `https://api.openweathermap.org/data/2.5/forecast?q=bangalore&APPID=${APIkey}&units=metric`;
+
+    Promise.all([fetch(weather), fetch(forecast)])
+      .then(([res1, res2]) => {
+        if (res1.ok && res2.ok) {
+          return Promise.all([res1.json(), res2.json()]);
+        }
+        throw Error(res1.statusText, res2.statusText);
+      })
+      .then(([data1, data2]) => {
+        
+        const currentDate = new Date();
+        const date = `${days[currentDate.getDay()]} ${currentDate.getDate()} ${
+          months[currentDate.getMonth()]
+        }`;
+        const sunset = new Date(data1.sys.sunset * 1000).toLocaleTimeString().slice(0, 5);
+        const sunrise = new Date(data1.sys.sunrise * 1000).toLocaleTimeString().slice(0, 5);
+
+        const weatherInfo = {
+          
+          city: data1.name,
+          country: data1.sys.country,
+          date,
+          description: data1.weather[0].description,
+          main: data1.weather[0].main,
+          temp: data1.main.temp,
+          highestTemp: data1.main.temp_max,
+          lowestTemp: data1.main.temp_min,
+          sunrise,
+          sunset,
+          clouds: data1.clouds.all,
+          humidity: data1.main.humidity,
+          wind: data1.wind.speed,
+          forecast: data2.list,
+        };
+        this.setState({
+          weatherInfo,
+          error: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+
+        this.setState({
+          error: true,
+          weatherInfo: null,
+        });
+      });
       })
     }
     
